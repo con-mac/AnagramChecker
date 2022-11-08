0f4f90ac98ec63f7e41c2a0747d52bb60f605d71
Conor Macklin <conor.macklin1986@gmail.com>
2022-11-08 20:28:03 +0000

    Libraries and structure

--- src/Anagram.java
+++ src/Anagram.java
@@ -1,6 +1,22 @@
 import java.util.*;
-
+//import text file libraries
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Anagram {
+
+    //File writer variables set to null
+    FileWriter fw = null;
+    BufferedWriter bw = null;
+    PrintWriter pw = null;
+
+    static void isAnagram(String s1, String s2)
+    {
+
+
+    }
+
 
 }
