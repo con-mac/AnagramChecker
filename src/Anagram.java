c43ed174787c7cfbd7f78f8a2b77f1b42e921733
Conor Macklin <conor.macklin1986@gmail.com>
2022-11-11 21:48:26 +0000

    Commented edit

--- src/Anagram.java
+++ src/Anagram.java
@@ -7,42 +7,46 @@ public class Anagram {
 
     static ArrayList<String> ar = new ArrayList<String>();
 
+
     //File writer variables set to null
     FileWriter fw = null;
     BufferedWriter bw = null;
     PrintWriter pw = null;
 
 
-
+//Function to check words for anagram match
     static void isAnagram(String s1, String s2) {
-        String copyOfs1 = s1;
-        String copyOfs2 = s2;
 
+        //Create copies to use for comparison
+        String copy1 = s1;
+        String copy2 = s2;
+
+
+
+        //Set boolean for comparison break
         boolean status = true;
 
-        if (copyOfs1.length() != copyOfs2.length()) {
+        if (copy1.length() != copy2.length()) {
 
 
             status = false;
         } else {
-            //Changing the case of characters of both copyOfs1 and copyOfs2 and converting them to char array
+            //Allow for Capital letters by changing the case of characters of both copy1 and copy2 and converting them to char array
 
-            char[] s1Array = copyOfs1.toLowerCase().toCharArray();
+            char[] s1Array = copy1.toLowerCase().toCharArray();
 
-            char[] s2Array = copyOfs2.toLowerCase().toCharArray();
+            char[] s2Array = copy2.toLowerCase().toCharArray();
 
             //Sorting both s1Array and s2Array
-
             Arrays.sort(s1Array);
 
             Arrays.sort(s2Array);
 
             //Checking whether s1Array and s2Array are equal
-
             status = Arrays.equals(s1Array, s2Array);
         }
 
-        //Output
+        //Output string
 
         if (status) {
             System.out.println("\n" + s1 + " and " + s2 + " are anagrams");
@@ -52,46 +56,38 @@ public class Anagram {
 
     }
 
-
+//Main method
     public static void main(String[] args) throws IOException {
+
+
+        //One of a number of methods used to try and read in text file and add to array list for comparison. Would appreciate feedback on solution
+//        try (Scanner s = new Scanner(new FileReader("C:\\Users\\conor\\OneDrive\\Ulster University\\Graduate Prep\\CME\\AnagramChecker\\src\\Anagrams.txt"))) {
+//            while (s.hasNext()) {
+//                ar.add(s.nextLine());
+//            }
+//        }
+
+        //Initiate boolean
         boolean validUsername = true;
+
+        //Validate against special characters
         String regexForNumbers = "^[^<>',\"/;`%]*$";
 
+        //pass in user input
         String username, word1, word2;
         Scanner s = new Scanner(System.in);
 
 
-//        try (FileReader f = new FileReader("C:\\Users\\conor\\OneDrive\\Ulster University\\Graduate Prep\\CME\\AnagramChecker\\src\\Anagrams.txt")) {
-//            StringBuffer sb = new StringBuffer();
-//            while (f.ready()) {
-//                char c = (char) f.read();
-//                if (c == '\n') {
-//                    ar.add(sb.toString());
-//                    sb = new StringBuffer();
-//                } else {
-//                    sb.append(c);
-//                }
-//            }
-//            if (sb.length() > 0) {
-//                ar.add(sb.toString());
-//            }
-//        }
-
-
-//        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\conor\\OneDrive\\Ulster University\\Graduate Prep\\CME\\AnagramChecker\\src\\Anagrams.txt"))) {
-//            while (br.ready()) {
-//                ar.add(br.readLine());
-//            }
-//        }
-
-
+        //While loop for user input break
         while (validUsername) {
 
             System.out.print("Enter username:");
             username = s.nextLine();
 
+            //add username to array list cache to check for previous entries
             ar.add(username);
 
+            //HashSet for Cache validation on username
             Set<String> store = new HashSet<>();
 
             for (String name : ar) {
@@ -102,18 +98,22 @@ public class Anagram {
                 }
             }
 
-
+            //Validate correct input (letters only, no spaces) using regex
             if (!username.matches("\\D+") || !username.matches(regexForNumbers)) {
                 System.err.println("Invalid user name supplied (" + username + ")!.\n"
                         + "Numerical digits, Special Characters, or Spaces are not permitted! Try again...");
                 validUsername = false;
             } else {
 
+
+                //First word input
                 System.out.print("Enter first word:");
                 word1 = s.nextLine();
 
+                //add first word to array list cache to check for previous entries
                 ar.add(word1);
 
+                //Validate entry
                 Set<String> store2 = new HashSet<>();
 
                 for (String name : ar) {
@@ -124,7 +124,6 @@ public class Anagram {
                     }
                 }
 
-
                 if (!word1.matches("\\D+") || !word1.matches(regexForNumbers)) {
                     System.err.println("Invalid word supplied (" + word1 + ")!.\n"
                             + "Numerical digits, Special Characters, or Spaces are not permitted! Try again...");
@@ -132,11 +131,14 @@ public class Anagram {
                 } else {
 
 
+                    //Second word input
                     System.out.print("Enter second word:");
                     word2 = s.nextLine();
 
+                    //Add to cache
                     ar.add(word2);
 
+                    //Validate
                     Set<String> store3 = new HashSet<>();
 
                     for (String name : ar) {
@@ -153,12 +155,15 @@ public class Anagram {
                         validUsername = false;
                     }
 
+                    //Call anagram method and compare
                     isAnagram(word1, word2);
 
 
+                    //Print anagram output
                     System.out.println("\nUser " + username + "'s Anagram:\n" + word1 + "\n" + word2);
 
 
+                    //Append username and anagram to text file
                     try (FileWriter f = new FileWriter("C:\\Users\\conor\\OneDrive\\Ulster University\\Graduate Prep\\CME\\AnagramChecker\\src\\Anagrams.txt", true);
                          BufferedWriter b = new BufferedWriter(f);
                          PrintWriter p = new PrintWriter(b);) {
